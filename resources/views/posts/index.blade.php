e5352fb759f2943372c825f09c0c86679f0218e9
tesarp1812 <tesar.akbar@student.unmer.ac.id>
2023-05-21 17:58:56 +0700

    first commit

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -30,47 +30,71 @@
 <div class="content mt-3">
  
     <div class="animated fadeIn">
-        <table class="table table-bordered">
-            <thead>
-                <tr>
-                    <th>NAMA</th>
-                    <th>UMUR</th>
-                    <th>AGAMA</th>
-                    <th>PENDIDIKAN</th>
-                    <th>ALAMAT</th>
-                    <th>NOMER TELEPON</th>
-                    <th>NAMA SUAMI</th>
-                    <th>UMUR SUAMI</th>
-                    <th>AGAMA SUAMI</th>
-                    <th>PENDIDIKAN SUAMI</th>
-                    <th>PEKERJAAN SUAMI</th>
-                    <th>ALAMAT SUAMI</th>
-                    <th>NOMER SUAMI</th>
-                    <th>NOMER TELEPON SUAMI</th>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($posts as $post)
-                <tr>
-                <td>{{ $post->nama }}</td>
-                <td>{{ $post->umur }}</td>
-                <td>{{ $post->agama }}</td>
-                <td>{{ $post->pendidikan }}</td>
-                <td>{{ $post->alamat }}</td>
-                <td>{{ $post->nomer_tlpn }}</td>
-                <td>{{ $post->nama_suami }}</td>
-                <td>{{ $post->umur_suami }}</td>
-                <td>{{ $post->agama_suami }}</td>
-                <td>{{ $post->penidikan_suami }}</td>
-                <td>{{ $post->pekerjaan_suami }}</td>
-                <td>{{ $post->alamat_suami }}</td>
-                <td>{{ $post->nomer_suami }}</td>
-                </tr>
-                
-                @endforeach
-            </tbody>
-        </table>
+
+        <div class="card">
+            <div class="card-header">
+                <div class="pull-left">
+                    <strong>Biodata</strong>
+                </div>
+                    <div class="pull-right">
+                        <a href="">
+                            <i class="fa fa-plus"></i> Tambah Pasien
+                        </a>
+                    </div>
+            </div>
+
+            <div class="card-body table-responsive">
+
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>NAMA</th>
+                            <th>UMUR</th>
+                            <th>AGAMA</th>
+                            <th>PENDIDIKAN</th>
+                            <th>ALAMAT</th>
+                            <th>NOMER TELEPON</th>
+                            <th>NAMA SUAMI</th>
+                            <th>UMUR SUAMI</th>
+                            <th>AGAMA SUAMI</th>
+                            <th>PENDIDIKAN SUAMI</th>
+                            <th>PEKERJAAN SUAMI</th>
+                            <th>ALAMAT SUAMI</th>
+                            <th>NOMER SUAMI</th>
+                            <th>NOMER TELEPON SUAMI</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($posts as $post)
+                        <tr>
+                        <td>{{ $post->id}}</td>
+                        <td>{{ $post->nama }}</td>
+                        <td>{{ $post->umur }}</td>
+                        <td>{{ $post->agama }}</td>
+                        <td>{{ $post->pendidikan }}</td>
+                        <td>{{ $post->alamat }}</td>
+                        <td>{{ $post->nomer_tlpn }}</td>
+                        <td>{{ $post->nama_suami }}</td>
+                        <td>{{ $post->umur_suami }}</td>
+                        <td>{{ $post->agama_suami }}</td>
+                        <td>{{ $post->penidikan_suami }}</td>
+                        <td>{{ $post->pekerjaan_suami }}</td>
+                        <td>{{ $post->alamat_suami }}</td>
+                        <td>{{ $post->nomer_suami }}</td>
+                        </tr>
+                        
+                        @endforeach
+                    </tbody>
+                </table>
+
+            </div>
+
+        </div>
+
+
+        
     </div>
 
 </div>
--- resources/views/subjektif/data.blade.php
+++
@@ -1,1 +0,0 @@
-subjektif data
