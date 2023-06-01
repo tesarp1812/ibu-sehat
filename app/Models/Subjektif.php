1b54617edf41cda689dafcc703aa7e577b6ea74d
tesarp1812 <tesar.akbar@student.unmer.ac.id>
2023-06-01 17:39:35 +0700

    first_commit

--- app/Models/Subjektif.php
+++ app/Models/Subjektif.php
@@ -7,7 +7,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class Subjektif extends Model
 {
+
     use HasFactory;
+    protected $table = "posts";
+    protected $primaryKey = "id";
+    protected $fillable = [
+        'id',
+        'posts_id',
+        'keluhan',
+        'riwayat_penyakit',
+        'riwayat_keluarga',
+        'riwayat_dulu',
+        'menikah_ke',
+        'usia_menikah',
+        'lama_menikah',
+        'HPHT',
+        'menarche',
+        'lama_haid',
+        'siklus_haid',
+        'warna',
+        'konsisten',
+        'flour_albus',
+        'riwayat_kb'
+    ];
 
     /**
      * post
